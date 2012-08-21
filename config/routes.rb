c5295e5188189eaf4e20a03fc596ef045ebbebbb
jessicaeldredge <jessica.eldredge@livingsocial.com>
2012-08-20 20:35:58 -0400

    Redirect root to the ideas index

--- config/routes.rb
+++ config/routes.rb
@@ -50,8 +50,7 @@ Railsgirls::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
+root :to => redirect("/ideas")
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
